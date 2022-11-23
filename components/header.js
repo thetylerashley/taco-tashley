17fc49b12bb8a89a69684b0356c1079ca13f2489
Tyler Ashley <tashley@tashley-mac.local>
2022-11-23 12:15:22 -0500

    header link fix

--- components/header.js
+++ components/header.js
@@ -13,7 +13,7 @@ return (
     <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
         <div className="flex items-center">
-            <a href="/">
+            <a href="#">
                 <span className="sr-only">Your Company</span>
                 <img className="h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=white" alt="" />
             </a>
