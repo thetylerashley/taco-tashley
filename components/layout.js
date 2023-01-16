7327c7ba6766b09be815d7cdb9b5d1dfee289b06
Tyler Ashley <tashley@tashley-mac.local>
2023-01-16 11:52:59 -0500

    favicon addition

--- components/layout.js
+++ components/layout.js
@@ -11,7 +11,13 @@ export default function Layout({ children, home }) {
   return (
     <div className="">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#173c34" />
+        <meta name="msapplication-TileColor" content="#173c34" />
+        <meta name="theme-color" content="#173c34" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
