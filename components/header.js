bfa623ba3ea343fad96df8f4d7e22dddd01472bb
Tyler Ashley <tashley@tashley-mac.local>
2023-01-16 11:50:54 -0500

    header logo link update

--- components/header.js
+++ components/header.js
@@ -14,10 +14,10 @@ return (
     <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
             <div className="flex items-center">
-                <a href="/">
+                <Link href="/">
                     <span className="sr-only">Your Company</span>
                     <Image width={132} height={56} className="h-10 w-auto" src="images/tyler-ashley-co_logo_white.svg" alt="" />
-                </a>
+                </Link>
                 {/* <div className="ml-10 hidden space-x-8 lg:block">
                 {navigation.map((link) => (
                     <Link key={link.name} href={link.href} className="text-base font-medium text-white hover:text-indigo-50">
